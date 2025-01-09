648cd9aacd877a5adf0f6d6097f1aa925b817ac1
Yifan Yu <markyu98@outlook.com>
2025-01-09 11:49:45 +0100

    add shared focal example

--- examples/shared_focal.py
+++ examples/shared_focal.py
@@ -6,7 +6,7 @@ import cv2
 import madpose
 from madpose.utils import get_depths, compute_pose_error
 
-sample_path = 'examples/image_pairs/0_scannet'
+sample_path = 'examples/image_pairs/1_eth3d'
 
 # Thresholds for reprojection and epipolar errors
 reproj_pix_thres = 8.0
@@ -57,17 +57,24 @@ depth_map1 = np.load(depth_1_file)
 depth0 = get_depths(image0, depth_map0, mkpts0)
 depth1 = get_depths(image1, depth_map1, mkpts1)
 
+# Compute the principal points
+pp0 = (np.array(image0.shape[:2][::-1]) - 1) / 2
+pp1 = (np.array(image1.shape[:2][::-1]) - 1) / 2
+pp = pp0
+
 # Run hybrid estimation
-pose, stats = madpose.HybridEstimatePoseScaleOffset(
+pose, stats = madpose.HybridEstimatePoseScaleOffsetSharedFocal(
                   mkpts0, mkpts1, 
                   depth0, depth1,
                   [depth_map0.min(), depth_map1.min()], 
-                  K0, K1, options, est_config
+                  pp0, pp1, options, est_config
               )
 # rotation and translation of the estimated pose
 R_est, t_est = pose.R(), pose.t()
 # scale and offsets of the affine corrected depth maps
 s_est, o0_est, o1_est = pose.scale, pose.offset0, pose.offset1
+# estimated shared focal length
+f_est = pose.focal
 
 # Load the GT Pose
 T_0to1 = np.array(info['T_0to1'])
@@ -75,24 +82,32 @@ T_0to1 = np.array(info['T_0to1'])
 # Compute the pose error
 err_t, err_R = compute_pose_error(T_0to1, R_est, t_est)
 
+# Compute the focal error
+f_mean = np.mean([K0[0, 0], K0[1, 1], K1[0, 0], K1[1, 1]])
+err_f = np.abs(f_est - f_mean) / f_mean
+
 print("--- Hybrid Estimation Results ---")
 print(f"Rotation Error: {err_R:.4f} degrees")
 print(f"Translation Error: {err_t:.4f} meters")
+print(f"Focal Error: {(err_f * 100):.2f}%")
 print(f"Estimated scale, offset0, offset1: {s_est:.4f}, {o0_est:.4f}, {o1_est:.4f}")
 
 # Run point-based estimation using PoseLib
 import poselib
 
 ransac_opt_dict = {'max_epipolar_error': epipolar_pix_thres, 'min_iterations': 1000, 'max_iterations': 10000}
-cam0_dict = {'model': 'PINHOLE', 'width': image0.shape[1], 'height': image0.shape[0], 'params': [K0[0, 0], K0[1, 1], K0[0, 2], K0[1, 2]]}
-cam1_dict = {'model': 'PINHOLE', 'width': image1.shape[1], 'height': image1.shape[0], 'params': [K1[0, 0], K1[1, 1], K1[0, 2], K1[1, 2]]}
-
-pose_ponly, stats = poselib.estimate_relative_pose(mkpts0, mkpts1, cam0_dict, cam1_dict, ransac_opt_dict)
-R_ponly, t_ponly = pose_ponly.R, pose_ponly.t
+img_pair, stats = poselib.estimate_shared_focal_relative_pose(mkpts0, mkpts1, pp, ransac_opt_dict)
+shared_focal_pose = img_pair.pose
+R_ponly, t_ponly = shared_focal_pose.R, shared_focal_pose.t
+f_ponly = img_pair.camera1.focal()
 
 # Compute the pose error
 err_t, err_R = compute_pose_error(T_0to1, R_ponly, t_ponly)
 
+# Compute the focal error
+err_f = np.abs(f_ponly - f_mean) / f_mean
+
 print("--- Point-based Estimation Results ---")
 print(f"Rotation Error: {err_R:.4f} degrees")
 print(f"Translation Error: {err_t:.4f} meters")
+print(f"Focal Error: {(err_f * 100):.2f}%")
